e3c6d6f62848afa5332e31f147f9e9e3a5a95ae6
OrangeX4 <318483724@qq.com>
2020-12-02 16:45:25 +0800

    Complete copy function

--- src/extension.ts
+++ src/extension.ts
@@ -1,36 +1,78 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+import { start } from 'repl'
 import * as vscode from 'vscode'
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "better-markdown-latex-shortcuts" is now active!')
-
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with registerCommand
     // The commandId parameter must match the command field in package.json
     context.subscriptions.push(
         vscode.commands.registerCommand('better-markdown-latex-shortcuts.copyToRight', () => {
-            vscode.window.showInformationMessage('Success to copy to right!');
+            let editor = vscode.window.activeTextEditor
+            if (!editor) { return }
+            let doc = editor.document
+            let sel = editor.selections
+            if (sel.length === 0) { return }
+            // editor.edit((edit) => {
+            //     let start = sel[0].start
+            //     if (!editor) { return }
+            //     let text = doc.getText(sel[0])
+            //     edit.insert(start, text)
+            //     editor.selection = new vscode.Selection(start, new vscode.Position(start.line, start.character + text.length))
+            // })
+            editor.edit((edit) => {
+                if (!editor) { return }
+                sel.forEach((selection) => {
+                    let start = selection.start
+                    let text = doc.getText(selection)
+                    edit.insert(start, text)
+                })
+            }).then(() => {
+                if (!editor) { return }
+                editor.selections = sel.map((selection) => {
+                    let end = selection.end
+                    let text = doc.getText(selection)
+                    return new vscode.Selection(end, new vscode.Position(end.line, end.character + text.length))
+                })
+            })
         })
     )
     context.subscriptions.push(
         vscode.commands.registerCommand('better-markdown-latex-shortcuts.copyToLeft', () => {
-            vscode.window.showInformationMessage('Success to copy to left!');
+            let editor = vscode.window.activeTextEditor
+            if (!editor) { return }
+            let doc = editor.document
+            let sel = editor.selections
+            if (sel.length === 0) { return }
+            editor.edit((edit) => {
+                if (!editor) { return }
+                sel.forEach((selection) => {
+                    let end = selection.end
+                    let text = doc.getText(selection)
+                    edit.insert(end, text)
+                })
+            }).then(() => {
+                if (!editor) { return }
+                editor.selections = sel.map((selection) => {
+                    let start = selection.start
+                    let text = doc.getText(selection)
+                    return new vscode.Selection(start, new vscode.Position(start.line, start.character + text.length))
+                })
+            })
         })
     )
     context.subscriptions.push(
         vscode.commands.registerCommand('better-markdown-latex-shortcuts.moveToRight', () => {
-            vscode.window.showInformationMessage('Success to move to right!');
+            vscode.window.showInformationMessage('Success to move to right!')
         })
     )
     context.subscriptions.push(
         vscode.commands.registerCommand('better-markdown-latex-shortcuts.moveToLeft', () => {
-            vscode.window.showInformationMessage('Success to move to left!');
+            vscode.window.showInformationMessage('Success to move to left!')
         })
     )
 }
